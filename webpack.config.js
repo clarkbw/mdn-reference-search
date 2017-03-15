e7851e98eb3e19dad63030c3675dcd54bb70e29b
Bryan Clark <clarkbw@gmail.com>
2017-03-15 13:54:18 +0100

    create omnibox module

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        query: {
-          presets: ["env"]
-        }
+        loader: "babel-loader"
       }
     ]
   }
