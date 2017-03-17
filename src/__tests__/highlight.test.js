895c965e1b74ca3f619c00afa8b5489043f80cef
Bryan Clark <clarkbw@gmail.com>
2017-03-17 16:45:00 +0100

    add jest testing
    Initial add for jest testing.
    Good test coverage over all modules except omnibox.handleInputChanged.
    Created yarn test and yarn run coverage to gather test coverage from
    jest.

---
+++ src/__tests__/highlight.test.js
@@ -0,0 +1,15 @@
+import { chromeHighlightMatch, firefoxHighlightMatch } from '../highlight';
+
+describe('highlight:chrome', () => {
+  it('should return the text matched', () => {
+    expect(chromeHighlightMatch('the test', 'the')).toEqual(
+      '<match>the</match> test'
+    );
+  });
+});
+
+describe('highlight:firefox', () => {
+  it('should return the same text as passed', () => {
+    expect(firefoxHighlightMatch('the test', 'the')).toEqual('the test');
+  });
+});
